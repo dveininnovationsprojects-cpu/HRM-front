b6cf3b96ca48ad773bd6068d2fe3516263c70879
kaviyagowsi <gowsikaviya0616@gmail.com>
2026-04-29 16:21:44 +0530

    fgfjhgk

--- src/api/apiConfig.js
+++ src/api/apiConfig.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:8080', // Backend properties padi port 8080 dhaan correct 
-    withCredentials: true, // JWT cookie handling-ku idhu dhaan mass 
+    baseURL: 'http://localhost:8080', 
+    withCredentials: true, 
 });
 
 export default api;
